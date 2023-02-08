909e4f9fc7f668c438d1afae854b1889798a79ca
sukhvinder-ssr <sukhvinderssr0@gmail.com>
2023-02-08 19:55:00 +0530

    Edit3

--- js/script.js
+++ js/script.js
@@ -106,3 +106,23 @@ function checkFlexGap() {
 checkFlexGap();
 
 // https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
+
+/////form
+const handleSubmit = (event) => {
+  event.preventDefault();
+
+  const myForm = event.target;
+  const formData = new FormData(myForm);
+
+  fetch("/", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams(formData).toString(),
+  })
+    .then(() =>
+      alert("Thank you for your submission Sukhvinder Singh reply you  Soon")
+    )
+    .catch((error) => alert(error));
+};
+
+document.querySelector("form").addEventListener("submit", handleSubmit);
